2c7468fe0a5a0667732591245dfeeedd3e2a88ed
abhinavv0612 <abhinav06tomar@gmail.com>
2024-06-22 02:17:46 +0530

    setup express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-import express from "express";
+import express from 'express'
+
+const app =express();
+
+//Routes
+//Http methods : Get , post
+app.get('/', (req,res,next) =>{
+    res.json({message : "Welcome to my app"})
+
+})
+export default app;
