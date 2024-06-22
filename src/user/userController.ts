441a836363fc5028170436005fc2311b19160728
abhinavv0612 <abhinav06tomar@gmail.com>
2024-06-22 14:35:38 +0530

    add controller

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,18 @@
 import { Request,Response,NextFunction } from "express"
+import createHttpError from "http-errors";
 
 const createUser = async(req:Request,res:Response,next:NextFunction) =>
 {
+    const{name , email ,password} = req.body;
+
+    //Validation
+if(!name||!email||!password){
+    const error = createHttpError("400" , "aLL fields are required");
+    return next(error);
+}
+    //Process
+
+    //Response
     res.json({
 message:"User created"
     })
