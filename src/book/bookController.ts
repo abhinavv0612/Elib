ad3dfe5a947803a350fa3217f4103c6627567731
abhinavv0612 <abhinav06tomar@gmail.com>
2024-06-23 14:46:16 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,8 +1,45 @@
 import { Request, Response } from "express";
-
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
+import createHttpError from "http-errors";
 const createBook = async (req: Request, res: Response) => {
   console.log("file", req.files);
+
+  
+  const files = req.files as{[fieldname:string]: Express.Multer.File[]};
+
+  const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+
+  const fileName = files.coverImage[0].filename;
+
+  const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName)
+try{
+  const uploadResult = await cloudinary.uploader.upload(filePath,{
+    filename_override : fileName,
+    folder : 'book-covers',
+    format:  coverImageMimeType,
+  })
+
+  const bookFileName = files.file[0].filename;
+  const bookFilePath = path.resolve(
+    __dirname,'../../public/data/uploads',bookFileName
+  )
+  const bookFileUplaodResult = await cloudinary.uploader.upload(bookFilePath,{
+    resource:'raw',
+    filename_override : bookFileName,
+    folder: 'book-pdfs',
+    format : 'pdf',
+  })
+  console.log("bookfileuploadresult", bookFileUplaodResult);
+  console.log(uploadResult);
   res.json({});
+}
+catch(err)
+{
+  console.log(err);
+  return next(createHttpError(500,"Error while uploadint the files"))
+}
+
 };
 
 export { createBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,25 +1,23 @@
-import path from 'node:path';
+import path from "node:path";
 
 import express from "express";
 import { createBook } from "./bookController";
 import multer from "multer";
 
-
 const bookRouter = express.Router();
 
 //store file locally --> upload on external storage then delete
 
-
 const upload = multer({
-  dest: path.join(__dirname, "Router"as string),
-  limits: { fileSize: 3e7 }, //30mb
+  dest: path.resolve(__dirname, '../../public/data/uploads'),
+  limits: { fileSize: 1e7 }, //10mb --> free cloudinary limit of size of file
 });
-
+  
 //routes
 bookRouter.post(
   "/",
   upload.fields([
-    { name: "coverImage", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },  //we are uploading image and pdf of the book on cloudinary
     { name: "file", maxCount: 1 },
   ]),
   createBook
